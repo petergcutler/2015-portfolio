8a1466dbbaf3e8212fd66ddc58b0750664541375
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-09-28 15:01:11 -0400

    updated build

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,3 @@
-console.log('hello');
-$('circle.node').click(function() {
-     $(this).toggleClass('.red');
-  
-     $(this).toggleClass('.blue');
-});
-
-$('body').addClass('.red');
-
-$('.animationclicker').click(function() {
-     $('circle.node').addClass("blue");
-});
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log( "ready!" );
-    console.log('hello');
-    $(this).toggleClass('foo');
-});
-
-$('.something').click(function() {
-  window.alert(message);
 });
